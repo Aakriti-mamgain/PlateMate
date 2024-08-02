79aad8a8eeda3c13f7777c4ad9fbf647d9ea92ba
Aakriti-mamgain <157624875+Aakriti-mamgain@users.noreply.github.com>
2024-08-02 14:47:20 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order for frontend
 const placeOrder = async (req,res) =>{
 
-    const frontend_url = "https://platemate-frontend.onrender.com"
+    const frontend_url = "https://platemate-n7pq.onrender.com"
     try {
         const newOrder = new orderModel({
             userId:req.body.userId,
